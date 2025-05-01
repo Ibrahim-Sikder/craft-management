19faca1f2eb4dcbf4b517294fd420d92686fe7de
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2025-05-02 00:45:34 +0600

    build

---
+++ src/app/(dashboardLayout)/dashboard/super_admin/staff/add/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StaffForm from '../_components/StaffForm';
+
+const page = () => {
+  return <StaffForm/>
+};
+
+export default page;
--- src/redux/api/tag-types.ts
+++ src/redux/api/tag-types.ts
@@ -1,19 +1,18 @@
 export const tagTypesList = [
   "student",
-  'teacher',
-  'meal-report',
-  'class',
-  'subject',
-  'user',
-  'class-report',
-  'todayLesson',
-  'todayTask',
-  'room',
-  'time-slot',
-  'section',
-  'daily-class-report'
-  
-  
+  "teacher",
+  "meal-report",
+  "class",
+  "subject",
+  "user",
+  "class-report",
+  "todayLesson",
+  "todayTask",
+  "room",
+  "time-slot",
+  "section",
+  "daily-class-report",
+  "staff",
 ] as const;
 
-export type TagType = typeof tagTypesList[number];
+export type TagType = (typeof tagTypesList)[number];
